58ae65b16af4b5a72307bd20ee7598e884845a14
KazmaArakaki <twi1ightmessenger1988@gmail.com>
2017-10-02 13:40:51 +0900

    スレッド作成機能途中

--- public/dev/script.js
+++ public/dev/script.js
@@ -11,6 +11,7 @@ function initMap() {
    */
   const mapContainer = document.getElementById("map");
   const getPositionButton = document.getElementById("button_getposition");
+  const createThreadButton = document.getElementById("button_createthread");
   
   /**
    * Google Mapの初期化
@@ -66,6 +67,27 @@ function initMap() {
     }, null, geolocationConfig);
   });
 
+  createThreadButton.addEventListener("click", event => {
+    map.setCenter({
+      "lat": user.position.latitude,
+      "lng": user.position.longitude
+    });
+
+    const createTheadDialog = new google.maps.InfoWindow({
+      "content": createThreadForm()
+    });
+    
+    const createTheadMarker = new google.maps.Marker({
+      "map": map,
+      "position": {
+        "lat": user.position.latitude,
+        "lng": user.position.longitude
+      }
+    });
+    
+    createTheadDialog.open(map, createTheadMarker);
+  });
+
   /**
    * 現在地取得ボタンにイベントリスナを追加
    */
@@ -73,6 +95,47 @@ function initMap() {
     map.setCenter({
       "lat": user.position.latitude,
       "lng": user.position.longitude
-    })
+    });
   });
+}
+
+function createThreadForm() {
+  const container = document.createElement("div");
+  const titleRow = document.createElement("div");
+  const titleLabel = document.createElement("label");
+  const titleInput = document.createElement("input");
+  const descriptionRow = document.createElement("div");
+  const descriptionLabel = document.createElement("label");
+  const descriptionInput = document.createElement("input");
+  const controllerRow = document.createElement("div");
+  const submitButton = document.createElement("button");
+  const cancelButton = document.createElement("button");
+
+  container.className = "form_createthread";
+  titleRow.className = "form_createthread_row";
+  titleLabel.className = "form_createthread_label";
+  titleInput.className = "form_createthread_input";
+  descriptionRow.className = "form_createthread_row";
+  descriptionLabel.className = "form_createthread_label";
+  descriptionInput.className = "form_createthread_input";
+  controllerRow.className = "form_createthread_row";
+  submitButton.className = "form_createthread_button -submit";
+  cancelButton.className = "form_createthread_button -cancel";
+
+  titleLabel.textContent = "タイトル：";
+  descriptionLabel.textContent = "説明："
+  submitButton.textContent = "作成する";
+  cancelButton.textContent = "作成しない";
+
+  container.appendChild(titleRow);
+  titleRow.appendChild(titleLabel);
+  titleRow.appendChild(titleInput);
+  container.appendChild(descriptionRow);
+  descriptionRow.appendChild(descriptionLabel);
+  descriptionRow.appendChild(descriptionInput);
+  container.appendChild(controllerRow);
+  controllerRow.appendChild(submitButton);
+  controllerRow.appendChild(cancelButton);
+
+  return container;
 }
